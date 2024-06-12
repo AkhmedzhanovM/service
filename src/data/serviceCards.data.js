df889329674014ecddd90928196a37a4ad3df9e0
AkhmedzhanovM <akhmedzhanovmohammed@gmail.com>
2024-06-12 22:42:42 +0500

    second commit

--- src/data/serviceCards.data.js
+++ src/data/serviceCards.data.js
@@ -1,48 +1,48 @@
 export const serviceCards = [
     {
-        icon: 'src/assets/strategy.svg',
+        icon: './src/assets/strategy.svg',
         title: 'СТРАТЕГИЯ',
         subtitle: 'Выстроим грамотную стратегию.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/analysis.svg',
+        icon: './src/assets/analysis.svg',
         title: 'АНАЛИЗ',
         subtitle: 'Проанализируем проблемы бизнеса.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/social.svg',
+        icon: './src/assets/social.svg',
         title: 'СОЦ СЕТИ',
         subtitle: 'Используем по максимуму соц сети.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/development.svg',
+        icon: './src/assets/development.svg',
         title: 'РАЗВИТИЕ',
         subtitle: 'Развивем каждый бизнес.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/content.svg',
+        icon: './src/assets/content.svg',
         title: 'КОНТЕНТ',
         subtitle: 'Только качественный контент.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/design.svg',
+        icon: './src/assets/design.svg',
         title: 'ДИЗАЙН UX',
         subtitle: 'Актуальный и красивый дизайн.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/target.svg',
+        icon: './src/assets/target.svg',
         title: 'ТАРГЕТИРОВАННАЯ РЕКЛАМА',
         subtitle: 'Создадим уникальный и удобный дзиайн.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        icon: 'src/assets/product.svg',
+        icon: './src/assets/product.svg',
         title: 'ПРОДУКТ',
         subtitle: 'Лучший продукт на рынке.',
         paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
